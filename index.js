f748204e53ff6cf65d2899080d97d08a86dfb9c0
SahilPandhade <sahilpandhade@rediffmail.com>
2023-10-06 18:57:18 +0530

    Updated cors

--- index.js
+++ index.js
@@ -12,22 +12,30 @@ const connectDB = require('./config/db.js')
 connectDB()
 const app = express()
 console.log(process.env.CLIENT_URL)
+// app.use(cors({
+//     origin:process.env.CLIENT_URL,
+//     credentials:true,
+// }));
 app.use(cors({
-    origin:process.env.CLIENT_URL,
-    credentials:true,
+    origin: 'https://project-management-frontend-fdz7.onrender.com',
+    credentials: true,
 }));
 
-
-app.use('/graphql',(req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-}, graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
-}))
+}));
+
+// app.use('/graphql',(req, res, next) => {
+//     //res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// }, graphqlHTTP({
+//     schema,
+//     graphiql: process.env.NODE_ENV === 'development'
+// }))
 
 
 app.listen(port, console.log(`Listening to PORT: ${port}`))
