a63d5ddc7ef3b15898ed51e5da69a3dbc3882a11
SahilPandhade <sahilpandhade@rediffmail.com>
2023-10-06 18:21:22 +0530

    Updated cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const colors = require('colors')
 require('dotenv').config()
-const {graphqlHTTP} = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 
 const schema = require('./schema/schema.js')
 const port = process.env.PORT || 5000
@@ -10,34 +10,16 @@ const connectDB = require('./config/db.js')
 
 //connect to db
 connectDB()
-const app  = express()
+const app = express()
 console.log(process.env.CLIENT_URL)
 app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true,
-    
+
 }));
-// app.use((req, res, next) => {
-//     res.setHeader(
-//       "Access-Control-Allow-Origin",
-//       process.env.CLIENT_URL
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader("Access-Control-Allow-Private-Network", true);
-//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//     res.setHeader("Access-Control-Max-Age", 7200);
-  
-//     next();
-//   });
-app.use('/graphql',(req, res, next) => {
+
+
+app.use('/graphql',cors(),(req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -49,4 +31,4 @@ app.use('/graphql',(req, res, next) => {
 }))
 
 
-app.listen(port,console.log(`Listening to PORT: ${port}`))
+app.listen(port, console.log(`Listening to PORT: ${port}`))
